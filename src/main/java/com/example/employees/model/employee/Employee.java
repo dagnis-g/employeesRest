7ac0a1911e8598a2e9951d9db771ce9275b42da7
DagnisGulbis <dagnisgulbis1@gmail.com>
2022-07-04 12:55:42 +0300

    removed comments Employee.java

--- src/main/java/com/example/employees/model/employee/Employee.java
+++ src/main/java/com/example/employees/model/employee/Employee.java
@@ -21,23 +21,18 @@ public class Employee {
     private Integer employeeNo;
 
     @Column(name = "birth_date")
-//    @NotNull(message = "no valid birth date provided")
-//    @Past
     private LocalDate birthDate;
 
     @Column(name = "first_name")
-//    @NotBlank
     private String firstName;
 
     @Column(name = "last_name")
-//    @NotBlank
     private String lastName;
 
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @Column(name = "hire_date")
-//    @NotNull(message = "no valid hire date provided")
     private LocalDate hireDate;
 
     @Override
